1553ccbb67a213beb7dd62f2efd29ee15115649f
Mateusz Krasucki <mateusz.krasucki@gmail.com>
2015-01-24 15:46:29 +0100

    Msisdn class skeleton declaration.

---
+++ src/Msisdn.php
@@ -0,0 +1,56 @@
+<?php
+
+    namespace MateuszKrasucki\Msisdn-resolver;
+    
+    class Msisdn 
+    {
+        private $msisdn_given = null;
+        private $msisdn = null;
+        private $country_code = null;
+        private $country_id = null;
+        private $mno = null;
+        
+        public function __construct($msisdn_given = null))
+        {
+        }
+        
+        public function set($msisdn_given = null)
+        {
+        }
+        
+        public function getFullString()
+        {
+        }
+        
+        public function getMsisdnGiven()
+        {
+        }
+        
+        public function getMsisdn()
+        {
+        }
+        
+        public function getCountryCode()
+        {
+        }
+        
+        public function getCountryId()
+        {
+        }
+        
+        public function getMno()
+        {
+        }
+        
+        private function processMsisdnGiven()
+        {
+        }
+        
+        private function matchCountryCodeAndValidate()
+        {
+        }
+        
+        private function matchMnoAndValidate()
+        {
+        }
+?>
